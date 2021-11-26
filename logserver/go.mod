40a44d37cc66494b052a1fe3da30f0125aa3e4c3
Nanik T <nanikjava@gmail.com>
2021-11-26 20:51:41 +1100

    Chapter 2 - Logserver

---
+++ logserver/go.mod
@@ -0,0 +1,11 @@
+module fitness.dev/app
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/kataras/golog v0.1.7
+)
---
+++ logserver/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/k0kubun/pp"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var router *mux.Router
+
+func main() {
+	log.Println("Initializing logging server at port 8010.")
+	runServer(":8010")
+}
+
+//runServer to run the logging server
+func runServer(addr string) {
+	router = mux.NewRouter()
+	initializeRoutes()
+
+	scheme := pp.ColorScheme{
+		String: pp.Green | pp.Bold,
+		Float:  pp.Black | pp.BackgroundWhite | pp.Bold,
+	}
+	pp.SetColorScheme(scheme)
+
+	log.Fatal(http.ListenAndServe(addr, router))
+}
+
+//respondWithError handle error response
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	response, _ := json.Marshal(message)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+//loghandler to handle log POST request
+func loghandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	defer r.Body.Close()
+
+	pp.Println(string(body))
+	w.WriteHeader(http.StatusCreated)
+}
+
+//initializeRoutes to initialize different routes
+func initializeRoutes() {
+	router.HandleFunc("/log", loghandler).Methods("POST")
+}
