52a14963cb2b9a5ff0b71c2458ec8673b9845476
Nanik T <nanikjava@gmail.com>
2022-01-14 23:45:07 +1100

    Add code for dynamic content
    ix

---
+++ chapter4/dynamic/go.mod
@@ -0,0 +1,5 @@
+module dynamic
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ chapter4/dynamic/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type staticHandler struct {
+	staticPath string
+	indexPage  string
+}
+
+func (h staticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	log.Println(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	result := "Login "
+	r.ParseForm()
+
+	if validateUser(r.FormValue("username"), r.FormValue("password")) {
+		result = result + "successfull"
+	} else {
+		result = result + "unsuccessful"
+	}
+
+	tpl := template.Must(template.ParseFiles("static/tmpl/msg.html"))
+
+	tpl.Execute(w, result)
+}
+
+func validateUser(username string, password string) bool {
+	return (username == "admin") && (password == "admin")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/login", postHandler).Methods("POST")
+
+	spa := staticHandler{staticPath: "static", indexPage: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:3333",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
