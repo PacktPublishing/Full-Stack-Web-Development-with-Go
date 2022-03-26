af1ebaa0e8168aad7db1c805a6731a1afeedd7fd
Nanik T <nanikjava@gmail.com>
2022-03-26 22:12:11 +1100

    Add chapter 7 router example

---
+++ chapter7/router/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <body class="bg-gray-900">
+  <div class="flex min-h-screen justify-center">
+    <div class="flex flex-col items-center justify-center">
+      <div class="flex flex-col space-x-2 items-start justify-start">
+        <p class="text-2xl font-semibold text-white">Home Screen</p>
+      </div>
+    </div>
+  </div>
+  </body>
+</template>
