22342d4bade29087b83ff5fac04e88356374db47
Nick Glynn <n.s.glynn@gmail.com>
2022-06-25 12:27:25 +1000

    Import frontend

---
+++ chapter 9/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">{{ msg }}</h1>
+  <div class="flex space-x-2 justify-center">
+    <button
+      @click="count++"
+      type="button"
+      class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+    >
+      Click me - my count is {{ count }}
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
