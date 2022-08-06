6a3016c46ec621ff8d0ad0c7b266cfdb762f61b9
Nanik T <nanikjava@gmail.com>
2022-08-06 21:15:47 +1000

    Modify chapter 7

--- chapter7/router/src/main.js
+++ chapter7/router/src/main.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
+import {createApp} from 'vue'
 import App from "./App.vue";
 import router from "./router";
 
-new Vue({
-    router,
-    render: h => h(App)
-}).$mount("#app");
+const myApp = createApp(App)
+myApp.use(router)
+
+myApp.mount('#app')
